57be0bd4b982c63ef47c9bd78b0439d4d7789097
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-02-13 12:25:51 +0100

    Ok… I give them

--- source/computer/CoolSystemStuff.hx
+++ source/computer/CoolSystemStuff.hx
@@ -2,28 +2,54 @@ package computer;
 
 // crazy system shit!!!!!
 // lordryan wrote this :) (erizur added cross platform env vars)
-// wither deleted it all, sorry but no!
+// wither made this more stable
 import sys.io.File;
 import sys.io.Process;
 import haxe.io.Bytes;
 
+/** BE CAREFUL!!! THIS COULD FLAG YOUR MOD AS VIRUS IF YOU DONT USE IT WISELY!!!!
+*/
 class CoolSystemStuff
 {
 	public static function getUsername():String
 	{
-		return 'no more, thanks';
+		if(!ClientPrefs.safeMode) {
+			// uhh this one is self explanatory
+			#if windows
+			return Sys.getEnv("USERNAME");
+			#else
+			return Sys.getEnv("USER");
+			#end
+		}
+		return 'NOPE';
 	}
 
 	public static function getUserPath():String
 	{
-		return 'no more, thanks';
+		if(!ClientPrefs.safeMode) {
+			// this one is also self explantory
+			#if windows
+			return Sys.getEnv("USERPROFILE");
+			#else
+			return Sys.getEnv("HOME");
+			#end
+		}
+		return 'NOPE';
 	}
 
 	public static function getTempPath():String
 	{
-		return 'no more, thanks';
+		if(!ClientPrefs.safeMode) {
+			// gets appdata temp folder lol
+			#if windows
+			return Sys.getEnv("TEMP");
+			#else
+			// most non-windows os dont have a temp path, or if they do its not 100% compatible, so the user folder will be a fallback
+			return Sys.getEnv("HOME");
+			#end
+		}
+		return 'NOPE';
 	}
-
 	public static function executableFileName()
 	{
 		#if windows
