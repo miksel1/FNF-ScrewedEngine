9433eba658b57a9535032fc1e7de37427230ed3a
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-02-18 17:18:09 +0100

    fix compiling

---
+++ source/gamejolt/GJKeys.hx
@@ -0,0 +1,10 @@
+package gamejolt;
+
+/**
+ * DONT BE HACKER DUDE
+ */
+class GJKeys
+{
+	public static var id:Int = 0; // Your Game ID
+	public static var key:String = ''; // Your Game Private Key
+}
