7800a79ec840123d624af438508070db46eb8726
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-05-03 00:21:04 -0400

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -19,8 +19,8 @@ import flixel.text.FlxText;
 import flixel.util.FlxColor;
 import flixel.tweens.FlxTween;
 import flixel.tweens.FlxEase;
-import flixel.addons.effects.chainable.FlxGlitchEffect;
-import flixel.graphics.frames.FlxFilterFrames;
+// import flixel.addons.effects.chainable.FlxGlitchEffect;
+// import flixel.graphics.frames.FlxFilterFrames;
 import openfl.filters.BitmapFilter;
 #if MODS_ALLOWED
 import sys.FileSystem;
