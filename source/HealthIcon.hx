1f59f1e69b8a5e044c26dfb48e27441cd489039b
Gabriela Bestial <93128334+BeastlyGabi@users.noreply.github.com>
2023-06-14 01:38:55 -0400

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -19,7 +19,6 @@ class HealthIcon extends FlxSprite
 		isOldIcon = (char == 'bf-old');
 		this.isPlayer = isPlayer;
 		changeIcon(char);
-		scrollFactor.set();
 	}
 
 	override function update(elapsed:Float)
@@ -30,57 +29,54 @@ class HealthIcon extends FlxSprite
 			setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
 	}
 
-	inline public function swapOldIcon() {
+	inline public function swapOldIcon():Void {
 		(isOldIcon = !isOldIcon) ? changeIcon('bf-old') : changeIcon('bf');
 	}
 
-	private var iconOffsets:Array<Float> = [0, 0];
-	public function changeIcon(char:String) {
+	private var iconOffsets:FlxPoint = new FlxPoint(0, 0);
+
+	public function changeIcon(char:String):Void {
 		if(this.char != char) {
 			var name:String = 'icons/' + char;
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
 			var file:Dynamic = Paths.image(name);
 
-			loadGraphic(file); //Load stupidly first for getting the file size
-			var frames:Array<Int> = [0, 1, 2];
-			var finalWidth = 3;
-			switch (file.width)
-			{
-				case 450:
-					finalWidth = 3;
-					frames = [0, 1, 2];
-					iconOffsets[0] = (width - 450) / 3;
-					iconOffsets[1] = (width - 450) / 3;
-				case 300:
-					finalWidth = 2;
-					frames = [0, 1];
-					iconOffsets[0] = (width - 300) / 2;
-					iconOffsets[1] = (width - 300) / 2;
-				case 150:
-					finalWidth = 1;
-					frames = [0];
-					iconOffsets[0] = (width - 150) / 1;
-			}
-			loadGraphic(file, true, Math.floor(width / finalWidth), Math.floor(height)); //Then load it fr
-			updateHitbox();
+			loadGraphic(file); //Get file size
+			loadGraphic(file, true, 150, 150); //Then load it fr
 
-			animation.add(char, frames, 0, false, isPlayer);
+			animation.add(char, [for (i in 0...frames.frames.length) i], 0, false, isPlayer);
 			animation.play(char);
 			this.char = char;
 
-			antialiasing = ClientPrefs.globalAntialiasing;
-			if(char.endsWith('-pixel')) {
-				antialiasing = false;
-			}
+			iconOffsets.set((width - 150) / frames.frames.length, (height - 150) / frames.frames.length);
+
+			antialiasing = (ClientPrefs.globalAntialiasing && !char.endsWith('-pixel'));
+			scrollFactor.set();
+			updateHitbox();
 		}
 	}
 
-	override function updateHitbox()
+	override function updateHitbox():Void
 	{
 		super.updateHitbox();
-		offset.x = iconOffsets[0];
-		offset.y = iconOffsets[1];
+		offset = iconOffsets;
+	}
+
+	/**
+	 * Store which health percentage will make a frame play
+	 * Percentage => Frame
+	 */
+	public var animationMap:Map<Int, Int> = [
+		0 => 0, // Default
+		20 => 1 // Lose
+		80 => 2 // Winning
+	];
+
+	public dynamic function updateFrame(health:Float):Void {
+		for (healthPercent => frame in animationMap)
+			if (health < healthPercent && animation.frames >= frame)
+				animation.curAnim.curFrame = frame;
 	}
 
 	inline public function getCharacter():String {
